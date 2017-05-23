14cb7f5365dad6492a7183adfc9c626085033204
Arthur Antunes <arthuragds@gmail.com>
2017-05-22 21:53:22 -0300

    Adding the code that makes possible for the shell to execute a program.

---
+++ xeu.cpp
@@ -0,0 +1,52 @@
+#include "xeu_utils/StreamParser.h"
+
+#include <iostream>
+#include <vector>
+#include <stdio.h> /* printf, scanf, NULL */
+#include <stdlib.h> /* malloc, free, rand */
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define gettid() syscall(SYS_gettid)
+
+using namespace xeu_utils;
+using namespace std;
+
+int main() {
+	
+	while(true) {
+		pid_t pid, t_pid;
+		int child_status;
+
+		pid = fork();
+
+		if(pid == -1) {
+			fprintf(stderr, "Fork failed!");
+		}
+
+		if(pid == 0) {
+			printf("%s=> ", getenv("USER"));
+			ParsingState p = StreamParser().parse();
+
+			std::vector<Command> commands = p.commands();
+
+			const char* filename = commands.front().filename();
+			char* const* argv = commands.front().argv();
+
+			execvp(filename, argv);
+		} else {
+			do {
+				t_pid = waitpid(pid, &child_status, WUNTRACED | WCONTINUED);
+				if(t_pid == -1) {
+					perror("waitpid");
+					exit(EXIT_FAILURE);
+				}	
+			} while(t_pid != pid);
+		}
+	}
+
+	return 0;
+}
