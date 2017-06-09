78caca7706598cd45e66bf938ed54faa05dcd75f
Arthur Antunes <arthuragds@gmail.com>
2017-06-08 22:32:26 -0300

    Listing the content of the current directory.

--- lab2/myps.cpp
+++ lab2/myps.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h> /* printf, scanf, NULL */
 #include <stdlib.h> /* malloc, free, rand */
+#include <dirent.h>
 
 int main()
 {
+	DIR *dir;
+	struct dirent *dp;
+
+	if((dir = opendir("./")) == NULL) {
+		perror("The directory cannot be open!");
+		exit(1);
+	}
+
+	while((dp = readdir(dir)) != NULL) {
+		puts(dp -> d_name);
+	}
+
     return (0);
 }
