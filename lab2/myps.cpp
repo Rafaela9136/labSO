bf39c567b9372bdc5917e10edcc4aed486c04725
Rafaela Lacerda de Araújo <rafaela9136@gmail.com>
2017-06-12 09:26:54 -0300

    Update myps.cpp
    Create a function to get all current processes pids.

--- lab2/myps.cpp
+++ lab2/myps.cpp
@@ -1,6 +1,44 @@
 #include <stdio.h> /* printf, scanf, NULL */
 #include <stdlib.h> /* malloc, free, rand */
 #include <dirent.h>
+#include <ctype.h>
+#include <vector>
+using std::vector; 
+
+vector<char*> getPids();
+
+int main()
+{
+	vector<char*> pids = getPids();
+    return (0);
+}
+
+vector<char*> getPids() {
+	DIR *dir;
+	struct dirent *dp;
+
+	if((dir = opendir("/proc/")) == NULL) {
+		perror("The directory cannot be open!");
+		exit(1);
+	}
+
+	vector<char*> pids;
+	char *pid;
+
+	while((dp = readdir(dir)) != NULL) {
+		pid = dp -> d_name;
+
+		if(isdigit(*pid)) {
+			pids.push_back(pid);
+		}
+	}
+
+	return pids;
+}
+
+/*#include <stdio.h> /* printf, scanf, NULL 
+#include <stdlib.h> /* malloc, free, rand 
+#include <dirent.h>
 
 int main()
 {
@@ -18,3 +56,4 @@ int main()
 
     return (0);
 }
+*/
