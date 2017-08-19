a95734b1f9de1688e33dcc1b8e27fecf2c50d09d
Arthur Antunes <arthuragds@gmail.com>
2017-08-18 21:18:10 -0300

    Adding lab5 folder to the repository structure.

---
+++ lab5/Simulator.java
@@ -0,0 +1,12 @@
+import algorithms.FirstComeFirstServed;
+import algorithms.ShortestSeekFirst;
+import algorithms.Antecipatory;
+import algorithms.Elevator;
+
+public class Simulator {
+
+	public static void main(String args[]) {
+		System.out.println("Bem vindo ao simulador de políticas de escalonamento de I/O!");
+	}
+
+}
