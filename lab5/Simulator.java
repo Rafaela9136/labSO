0b57daddbf8c5c09d2b5cd1c7afedb8fb80e5a0a
Arthur Antunes <arthuragds@gmail.com>
2017-08-26 13:29:01 -0300

    Adding the implementation of the First-Come First-Out algorithm to the project.

--- lab5/Simulator.java
+++ lab5/Simulator.java
@@ -2,35 +2,76 @@ import java.util.*;
 import algorithms.*;
 
 public class Simulator {
-	static Queue queue = new LinkedList();
+
+	/*
+	Estrutura para representar os cilindros do disco.
+	A lista possui 80 posições.
+	*/
+	static List<Integer> diskCylinders = new ArrayList<Integer>(80);
+
+	/*
+	Estrutura para armazenar as requisições ao disco.
+	*/
+	static List<Integer> requests = new ArrayList<Integer>();
 	static Random rdm = new Random();
 
 	public static void main(String args[]) {
 		System.out.println("Bem vindo ao simulador de políticas de escalonamento de I/O!");
-		populateQueue(); //Popula o queue com números randômicos
-		System.out.println("Disco: " + queue.toString());
+
+		initializeDisk();
+		initializeRequestList();
+
+		System.out.println("Requisições: " + requests.toString());
+
+		System.out.println("Executando o First-Come First-Served..");
+		FirstComeFirstServed fcfs = new FirstComeFirstServed(diskCylinders, requests);
+		fcfs.processAllRequests();
+
+		System.out.println("Número de movimentos do braço: " + fcfs.getNumberArmMotions());
+
+		/*
+		initializeRequestList(); //Popula o queue com números randômicos
+
+		System.out.println("Disco: " + requests.toString());
+		System.out.println("Tamanho do disco: " + requests.size());
 
 		runAlgorithm(); //Seleciona e executa o algoritmo
+		*/
 	}
 
-	public static void populateQueue() {
-		for (int i = 0;i < 10; i++)
-			queue.add(rdm.nextInt(60));
-	};
+	/*
+	Inicializa a lista de requisições com 10 números randômicos.
+	*/
+	public static void initializeRequestList() {
+		for (int i = 0;i < 10; i++) {
+			requests.add(rdm.nextInt(80));
+		}
+	}
 
+	/*
+	Inicializa as 80 posições do disco com zeros.
+	*/
+	public static void initializeDisk() {
+		for(int i = 0; i < 80; i++) {
+			diskCylinders.add(i, 0);
+		}
+	}
+
+	/*
 	public static void runAlgorithm() {
-		Algorithm[] algs = new Algorithm[] {new FirstComeFirstServed(), new ShortestSeekFirst(),
+		Algorithm[] algs = new Algorithm[] {new FirstComeFirstServed(diskCylinders, requests), new ShortestSeekFirst(),
 											new Elevator(), new Antecipatory()};
 		for (Algorithm alg : algs) {
 			System.out.println("\nSimulando " + alg.getClass().getSimpleName());
-			Algorithm algorithm = alg;
 
-			sequenceOfSeeks();//Imprime a sequência de seeks do algoritmo
+			Algorithm algorithm = alg; //?
+
+			sequenceOfSeeks();//Imprime a sequência de seeks do algoritmo //?
 		}
-
-	};
+	}
+	*/
 
 	public static void sequenceOfSeeks(){
 		//TODO
-	};
+	}
 }
--- lab5/algorithms/FirstComeFirstServed.java
+++ lab5/algorithms/FirstComeFirstServed.java
@@ -1,5 +1,56 @@
 package algorithms;
 
-public class FirstComeFirstServed implements Algorithm {
+import java.util.*;
+
+public class FirstComeFirstServed implements Algorithm{
+
+	private int numberArmMotions;
+	private int currentPosition;
+
+	private List<Integer> requests;
+	private List<Integer> diskCylinders;
+
+	public FirstComeFirstServed(List<Integer> diskCylinders, List<Integer> requests) {
+		this.requests = requests;
+		this.diskCylinders = diskCylinders;
+
+		this.numberArmMotions = 0;
+		this.currentPosition = 0;
+	}
+
+	/*
+	Processa todas as requisições.
+	*/
+	public void processAllRequests() {
+		for(Integer request : requests) {
+			processRequest(request);
+		}
+	}
+
+	/*
+	Processa uma requisição.
+	*/
+	public void processRequest(Integer request) {
+		if(request > 0 && request < diskCylinders.size()) {
+			if(request >= currentPosition) {
+				for(int i = currentPosition + 1; i <= request; i++) {
+					numberArmMotions++;
+				}
+			} else {
+				for(int i = currentPosition - 1; i >= request; i--) {
+					numberArmMotions++;
+				}
+			}
+			currentPosition = request;
+		}
+	}
+
+	/*
+	Retorna a quantidade de vezes que o braço do disco precisou se
+	mover para atender a todas as requisições.
+	*/
+	public int getNumberArmMotions() {
+		return numberArmMotions;
+	}
 
 }
